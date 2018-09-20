645b46b4f1e50834ac291d944aee339264bc0491
leozhi819 <738418971@qq.com>
2018-09-20 13:38:23 +0800

    墨

--- MoApp/app/src/main/java/top/leozhi/moapp/MainActivity.java
+++ MoApp/app/src/main/java/top/leozhi/moapp/MainActivity.java
@@ -8,12 +8,25 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import top.leozhi.moapp.utils.StatusBarUtil;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.design.widget.NavigationView;
+import android.widget.SearchView.*;
+import android.view.View;
+import android.view.MenuItem;
+import java.util.List;
+import java.util.ArrayList;
 
 
-public class MainActivity extends BaseActivity 
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener
 {
 	private DrawerLayout mDrawerLayout;
 	private Toolbar mToolbar;
+	private FloatingActionButton fab;
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+	private NavigationView mNavigationView;
 	
 	private int mStatusBarColor;
     
@@ -23,18 +36,54 @@ public class MainActivity extends BaseActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 		
-		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-		mToolbar = (Toolbar) findViewById(R.id.toolbar);
-		setSupportActionBar(mToolbar);
-		
-		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.navigation_drawer_open,
-																 R.string.navigation_drawer_close);
-		toggle.syncState();
+		initView();
+		initViewPager();
 	}
 	
 	@Override
     protected void setStatusBar() {
         mStatusBarColor = getResources().getColor(R.color.colorPrimary);
         StatusBarUtil.setColorForDrawerLayout(this, (DrawerLayout) findViewById(R.id.drawer_layout), mStatusBarColor, 0);
+	}
+	
+	public void initView(){
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mToolbar = (Toolbar) findViewById(R.id.toolbar);
+		mNavigationView = (NavigationView) findViewById(R.id.navigation);
+		fab = (FloatingActionButton) findViewById(R.id.fab_main);
+		setSupportActionBar(mToolbar);
+
+		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.navigation_drawer_open,
+																 R.string.navigation_drawer_close);
+		toggle.syncState();
+		
+		mNavigationView.setNavigationItemSelectedListener(this);
+        mNavigationView.setItemIconTintList(null);
+	}
+	
+	public void initViewPager(){
+		mTabLayout = (TabLayout) findViewById(R.id.tab_layout_main);
+        mViewPager = (ViewPager) findViewById(R.id.view_pager_main);
+		List<String> titles = new ArrayList<>();
+        titles.add(getString(R.string.tab_title_main_1));
+        titles.add(getString(R.string.tab_title_main_2));
+        titles.add(getString(R.string.tab_title_main_3));
+        mTabLayout.addTab(mTabLayout.newTab().setText(titles.get(0)));
+        mTabLayout.addTab(mTabLayout.newTab().setText(titles.get(1)));
+        mTabLayout.addTab(mTabLayout.newTab().setText(titles.get(2)));
+		
+	}
+	
+	@Override
+	public boolean onNavigationItemSelected(MenuItem menu)
+	{
+		
+		return false;
+	}
+	
+	@Override
+	public void onClick(View v)
+	{
+	
 	}
 }
